7f0807e7d08297cff83a41946cfd5317a5a496fa
Clément <clement.chazalet@gmail.com>
2021-03-23 15:49:10 +0100

    23 mars

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,41 @@
+import { getCurrencySymbol } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { reduce } from 'rxjs-compat/operator/reduce';
+import { AppareilService } from '../services/appareil.service';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.scss']
+})
+export class AppareilComponent implements OnInit {
+  
+  @Input() appareilName: string;
+  @Input() appareilStatus: string;
+  @Input() indexOfAppareil: number;
+
+  constructor(private appareilService: AppareilService) {
+    
+  }
+  ngOnInit(): void {
+  }
+
+  getStatus(){
+    return this.appareilStatus;
+  }
+
+  getColor() {
+    if(this.appareilStatus === 'allumé') {
+      return 'green';
+    } else if(this.appareilStatus === 'éteint') {
+      return 'red';
+    }
+  }
+
+  onSwitchOn(){
+    this.appareilService.switchOnOne(this.indexOfAppareil);
+  }
+  onSwitchOff(){
+    this.appareilService.switchOffOne(this.indexOfAppareil);
+  }
+}
